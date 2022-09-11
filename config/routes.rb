22ca942a9231040bd984e4361d590edb967832cb
ahiles <ogol.ac@gmail.com>
2022-09-11 19:20:59 +0300

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    root 'menu#index'
     devise_for :admins
     resources :categories
     resources :dishes
@@ -7,6 +8,6 @@ Rails.application.routes.draw do
     resources :day_menu_dishes
     resources :orders
     
-  	root 'menu#index'
+  	
   end
 end
